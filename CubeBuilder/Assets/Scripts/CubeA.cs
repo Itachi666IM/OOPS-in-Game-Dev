80f1bee9baa3920c682fef3e288d2d181c5d7f21
Itachi666IM <rannenchi009sharingantoad2@gmail.com>
2024-06-11 18:39:59 +0530

    Added Material List and Inheritance features

---
+++ CubeBuilder/Assets/Scripts/CubeA.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//INHERITANCE
+public class CubeA : Cube
+{
+    public override void SetCubeName()
+    {
+        cubeName = "CubeA";
+    }
+
+}
